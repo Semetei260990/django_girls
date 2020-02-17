98b44cd02726586b55de316f07d6328ae3ecf61d
Semetei <you@example.com>
2020-02-17 11:01:47 +0600

    My Django Girls app, first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/semetei.abaildaev/Documents/evening_bootcamp/homework/part6/django_girls/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
